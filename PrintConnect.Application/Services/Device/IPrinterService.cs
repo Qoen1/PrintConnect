ebbbf8c53fd912881318afcd560e9f4dbcdd3999
Qoen1 <kverstappen@protonmail.com>
2026-06-10 16:40:44 +0200

    finish merge info bar with main

--- PrintConnect.Application/Services/Device/IPrinterService.cs
+++ PrintConnect.Application/Services/Device/IPrinterService.cs
@@ -1,8 +1,10 @@
 using FluentResults;
+using PrintConnect.Domain.Entities;
 
 namespace PrintConnect.Application.Services.Device;
 
 public interface IPrinterService
 {
     public Task<Result<Domain.Entities.Job>> StartNextJobInQueueByIdAsync(Guid printerId);
+    public Task<Printer?> GetPrinterInfoById(Guid printerId);
 }
--- PrintConnect.Client/Pages/PrinterNavBar.razor.cs
+++ PrintConnect.Client/Pages/PrinterNavBar.razor.cs
@@ -5,11 +5,6 @@ namespace PrintConnect.Client.Pages;
 
 public partial class PrinterNavBar : ComponentBase
 {
-    private Printer? Printer { get; set; } = Printer.Create("http://192.168.178.158/", "maker", "WpZhQTj86AKwX4z",
-        "ger-de-printer", "je moeder", "prusa mk4s");
-
-    private void SetPrinterReady()
-    {
-        throw new NotImplementedException();
-    }
+    [Parameter]
+    public Printer? Printer { get; set; }
 }
